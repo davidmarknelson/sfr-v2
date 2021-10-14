62201759c0e500bf189d63fdd5a8971b7d45713f
David Nelson <nelsondavidmark@gmail.com>
2021-10-14 17:27:54 -0500

    Set up theme.

--- src/app/ui/view/view.module.ts
+++ src/app/ui/view/view.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { SfrHeaderComponent } from './header/header.component';
-import { SfrFooterComponent } from './footer/footer.component';
-import { SfrViewComponent } from './view/view.component';
 import { RouterModule } from '@angular/router';
+import { SfrFooterComponent } from './footer/footer.component';
+import { SfrHeaderComponent } from './header/header.component';
+import { SfrViewComponent } from './view/view.component';
 
 @NgModule({
   declarations: [SfrHeaderComponent, SfrFooterComponent, SfrViewComponent],
