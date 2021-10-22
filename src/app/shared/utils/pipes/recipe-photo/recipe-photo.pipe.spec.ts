4fb3fe5a2709c477f3436d1bed61261f6311a31c
David Nelson <nelsondavidmark@gmail.com>
2021-10-22 13:13:40 -0500

    Add pagination unit tests.

--- src/app/shared/utils/pipes/recipe-photo/recipe-photo.pipe.spec.ts
+++ src/app/shared/utils/pipes/recipe-photo/recipe-photo.pipe.spec.ts
@@ -2,9 +2,6 @@ import { SfrRecipePhotoPipe } from './recipe-photo.pipe';
 
 describe('RecipePhotoPipe', () => {
   const pipe = new SfrRecipePhotoPipe();
-  it('create an instance', () => {
-    expect(pipe).toBeTruthy();
-  });
 
   it('should return the provided value if it there is text', () => {
     expect(pipe.transform('photo-path')).toEqual('photo-path');
--- src/app/shared/utils/services/pagination/pagination.service.spec.ts
+++ src/app/shared/utils/services/pagination/pagination.service.spec.ts
@@ -1,16 +1,51 @@
 import { TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Observable, of } from 'rxjs';
+import { SfrPaginationService } from './pagination.service';
 
-import { PaginationService } from './pagination.service';
-
-describe('PaginationService', () => {
-  let service: PaginationService;
+describe('SfrPaginationService', () => {
+  let service: SfrPaginationService;
+  let ActivatedRouteMock: {
+    queryParamMap: Observable<{
+      page?: any;
+    }>;
+  } = {
+    queryParamMap: of({ page: '5' }),
+  };
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(PaginationService);
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: ActivatedRouteMock,
+        },
+      ],
+    }).compileComponents();
+    service = TestBed.inject<SfrPaginationService>(SfrPaginationService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  describe('getPageFromRoute$', () => {
+    it('should return 5 when the query param page value is 5', () => {
+      service.getPageFromRoute$.subscribe((page) => {
+        expect(page).toEqual(5);
+      });
+    });
+
+    it('should return 1 when there are no query params', () => {
+      ActivatedRouteMock = { queryParamMap: of({}) };
+      service.getPageFromRoute$.subscribe((page) => {
+        expect(page).toEqual(1);
+      });
+    });
+  });
+
+  describe('getSkip', () => {
+    it('should return the skip value for pagination', () => {
+      expect(service.getSkip(1)).toEqual(0);
+      expect(service.getSkip(2)).toEqual(9);
+    });
   });
 });
