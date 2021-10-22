1919e430e59b30360992614299a69e91fd673f7d
David Nelson <nelsondavidmark@gmail.com>
2021-10-21 19:24:20 -0500

    Add recipe photo pipe test.

--- src/app/shared/utils/pipes/recipe-photo/recipe-photo.pipe.spec.ts
+++ src/app/shared/utils/pipes/recipe-photo/recipe-photo.pipe.spec.ts
@@ -1,8 +1,18 @@
-import { RecipePhotoPipe } from './recipe-photo.pipe';
+import { SfrRecipePhotoPipe } from './recipe-photo.pipe';
 
 describe('RecipePhotoPipe', () => {
+  const pipe = new SfrRecipePhotoPipe();
   it('create an instance', () => {
-    const pipe = new RecipePhotoPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should return the provided value if it there is text', () => {
+    expect(pipe.transform('photo-path')).toEqual('photo-path');
+  });
+
+  it('should return the default recipe photo path if it there is not text', () => {
+    expect(pipe.transform(undefined)).toEqual(
+      '/assets/images/defaults/default-recipe-pic.jpg'
+    );
   });
 });
