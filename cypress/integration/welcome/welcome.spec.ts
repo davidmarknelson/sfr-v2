0f05fae8cc1fe99ebfeb28d6b8c7dd479b28512e
David Nelson <nelsondavidmark@gmail.com>
2021-10-25 18:36:41 -0500

    Finish welcome e2e tests.

--- cypress/integration/welcome/welcome.spec.ts
+++ cypress/integration/welcome/welcome.spec.ts
@@ -3,8 +3,13 @@ describe('Welcome page', () => {
     cy.visit('http://localhost:4200/welcome');
   });
 
-  it('should link to the recipes page in the page', () => {
+  it('should link to the recipes page in the page in the first main button', () => {
     cy.get('.cta a').first().click();
+    cy.url().should('eq', 'http://localhost:4200/recipes');
+  });
+
+  it('should link to the recipes page in the page in the last main button', () => {
+    cy.get('.cta a').last().click();
     cy.url().should('eq', 'http://localhost:4200/recipes');
   });
 });
