9725df4486d7057c4c819c256f4cc2ce606c8410
David Nelson <nelsondavidmark@gmail.com>
2021-10-26 14:18:27 -0500

    Add api url in environment file.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { InMemoryCache } from '@apollo/client/core';
 import { APOLLO_OPTIONS } from 'apollo-angular';
 import { HttpLink } from 'apollo-angular/http';
+import { environment } from 'src/environments/environment';
 import { SfrAppRoutingModule } from './app-routing.module';
 import { SfrAppComponent } from './app.component';
 import { PaginationDefault } from './shared/utils';
@@ -27,9 +28,9 @@ import { SfrViewModule } from './view/view.module';
         return {
           cache: new InMemoryCache(),
           link: httpLink.create({
-            uri: 'http://localhost:3000/graphql',
+            uri: `${environment.apiUrl}/graphql`,
             headers: new HttpHeaders({
-              'Access-Control-Allow-Credentials': 'http://localhost:3000',
+              'Access-Control-Allow-Credentials': `${environment.apiUrl}`,
             }),
           }),
         };
