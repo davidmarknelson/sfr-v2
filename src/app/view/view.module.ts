cf5f945d18a659e5358fc947d0a246e752d2872a
David Nelson <nelsondavidmark@gmail.com>
2021-11-05 12:04:48 -0500

    Move palette to its own file.

--- src/app/view/view.module.ts
+++ src/app/view/view.module.ts
@@ -1,14 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
 import { SfrUiContainerModule } from '@sfr/shared/ui';
+import { SfrRoundedButtonModule } from '@sfr/shared/utils/directives';
 import { SfrFooterComponent } from './footer/footer.component';
 import { SfrHeaderComponent } from './header/header.component';
 import { SfrViewComponent } from './view/view.component';
 
 @NgModule({
   declarations: [SfrHeaderComponent, SfrFooterComponent, SfrViewComponent],
-  imports: [CommonModule, RouterModule, MatToolbarModule, SfrUiContainerModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatToolbarModule,
+    SfrUiContainerModule,
+    MatButtonModule,
+    SfrRoundedButtonModule,
+  ],
 })
 export class SfrViewModule {}
