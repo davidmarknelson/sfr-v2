b145173de74b1719ad1cf2d6e9b4b2944a5de3c2
David Nelson <nelsondavidmark@gmail.com>
2021-12-08 13:15:50 -0500

    Initial signup spec setup.

--- src/app/features/auth/feature/signup/signup.component.spec.ts
+++ src/app/features/auth/feature/signup/signup.component.spec.ts
@@ -1,20 +1,67 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MATERIAL_SANITY_CHECKS } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { RouterTestingModule } from '@angular/router/testing';
+import { SignupGQL } from '@sfr/data-access/generated';
+import {
+  SfrUiAnnouncementModule,
+  SfrUiContainerModule,
+  SfrUiPageTitleModule,
+} from '@sfr/shared/ui';
+import { SfrRoundedButtonModule } from '@sfr/shared/utils/directives';
+import { SfrAuthService } from '@sfr/shared/utils/services';
+import { of } from 'rxjs';
+import { SfrSignupComponent } from './signup.component';
 
-import { SignupComponent } from './signup.component';
+let signupData: any;
 
 describe('SignupComponent', () => {
-  let component: SignupComponent;
-  let fixture: ComponentFixture<SignupComponent>;
+  let component: SfrSignupComponent;
+  let fixture: ComponentFixture<SfrSignupComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SignupComponent ]
-    })
-    .compileComponents();
+      declarations: [SfrSignupComponent],
+      imports: [
+        RouterTestingModule,
+        SfrUiContainerModule,
+        SfrUiPageTitleModule,
+        SfrRoundedButtonModule,
+        SfrUiAnnouncementModule,
+        MatFormFieldModule,
+        ReactiveFormsModule,
+        MatInputModule,
+        MatIconModule,
+        FlexLayoutModule,
+      ],
+      providers: [
+        { provide: MATERIAL_SANITY_CHECKS, useValue: false },
+        {
+          provide: SignupGQL,
+          useValue: {
+            mutate: () => {
+              return of(signupData);
+            },
+          },
+        },
+        {
+          provide: SfrAuthService,
+          useValue: {
+            authenticate: jest.fn(() => {
+              return;
+            }),
+          },
+        },
+      ],
+    }).compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(SignupComponent);
+    fixture = TestBed.createComponent(SfrSignupComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
