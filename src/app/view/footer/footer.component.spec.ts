8cf8317033718891401d01acbb2798c9b616739c
David Nelson <nelsondavidmark@gmail.com>
2022-03-14 13:53:05 -0400

    Add more footer links and unit tests.

--- src/app/view/footer/footer.component.spec.ts
+++ src/app/view/footer/footer.component.spec.ts
@@ -1,23 +1,74 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { MockAuthService } from '@sfr-testing/mocks';
+import { SfrAuthService } from '@sfr/shared/utils/services';
+import { of } from 'rxjs';
 import { SfrFooterComponent } from './footer.component';
 
 describe('SfrFooterComponent', () => {
-  let component: SfrFooterComponent;
   let fixture: ComponentFixture<SfrFooterComponent>;
+  let authService: SfrAuthService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [SfrFooterComponent],
+      providers: [
+        {
+          provide: SfrAuthService,
+          useClass: MockAuthService,
+        },
+      ],
     }).compileComponents();
+    authService = TestBed.inject(SfrAuthService);
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SfrFooterComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  describe('authenticated', () => {
+    let authSpy: any;
+
+    beforeEach(() => {
+      authSpy = jest
+        .spyOn(authService, 'isAuthenticated$', 'get')
+        .mockReturnValue(of(true));
+      fixture = TestBed.createComponent(SfrFooterComponent);
+      fixture.detectChanges();
+    });
+
+    it('should show the signup and login links', () => {
+      expect(authSpy).toBeCalled();
+      expect(
+        fixture.debugElement.query(By.css('[routerLink="/signup"]'))
+      ).toBeFalsy();
+      expect(
+        fixture.debugElement.query(By.css('[routerLink="/login"]'))
+      ).toBeFalsy();
+      expect(
+        fixture.debugElement.query(By.css('[routerLink="/profile"]'))
+      ).toBeTruthy();
+    });
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('unauthenticated', () => {
+    let authSpy: any;
+
+    beforeEach(() => {
+      authSpy = jest
+        .spyOn(authService, 'isAuthenticated$', 'get')
+        .mockReturnValue(of(false));
+      fixture = TestBed.createComponent(SfrFooterComponent);
+      fixture.detectChanges();
+    });
+
+    it('should show the signup and login links', () => {
+      expect(authSpy).toBeCalled();
+      expect(
+        fixture.debugElement.query(By.css('[routerLink="/signup"]'))
+      ).toBeTruthy();
+      expect(
+        fixture.debugElement.query(By.css('[routerLink="/login"]'))
+      ).toBeTruthy();
+      expect(
+        fixture.debugElement.query(By.css('[routerLink="/profile"]'))
+      ).toBeFalsy();
+    });
   });
 });
--- src/app/view/footer/footer.component.ts
+++ src/app/view/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { SfrAuthService } from '@sfr/shared/utils/services';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'sfr-footer',
@@ -6,4 +8,10 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./footer.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SfrFooterComponent {}
+export class SfrFooterComponent {
+  isAuthenticated$: Observable<boolean>;
+
+  constructor(private readonly auth: SfrAuthService) {
+    this.isAuthenticated$ = this.auth.isAuthenticated$;
+  }
+}
